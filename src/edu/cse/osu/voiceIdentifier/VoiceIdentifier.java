a6501df0f64c259014911fb19bd480b11cb0c0ce
dalbz <david.albert09@gmail.com>
2013-02-14 13:35:29 -0500

    Do simple test of audio library to get frequency-time data

--- src/edu/cse/osu/voiceIdentifier/VoiceIdentifier.java
+++ src/edu/cse/osu/voiceIdentifier/VoiceIdentifier.java
@@ -1,8 +1,28 @@
 package edu.cse.osu.voiceIdentifier;
 
+import com.musicg.wave.Wave;
+import com.musicg.wave.extension.Spectrogram;
+
 public class VoiceIdentifier {
 
     public static void main(String[] args) {
+        
+        // http://www.wavlist.com/movies/317/ofsp-suprman3.wav
+        
+        String filename = "data/ofsp-suprman3.wav";
+
+        // create a wave object
+        Wave wave = new Wave(filename);
+        Spectrogram spectrogram = new Spectrogram(wave);
+
+        double[][] freqTimeData = spectrogram.getNormalizedSpectrogramData();
+
+        for(int i = 0; i < freqTimeData.length; i++){
+            for(int j = 0; j < freqTimeData[0].length; j++){
+                System.out.format("%f.2 ", freqTimeData[i][j]);
+            }
+            System.out.println();
+        }
 
     }
 
